ea8d77664bca9cc6a96823ab8a1f39fedd738863
Pietro <pietrodig@live.com>
2023-05-01 23:25:39 +0200

    Add engrampa package

--- easy-arch.sh
+++ easy-arch.sh
@@ -60,7 +60,7 @@ sudo pacman -S --needed ntp zim libreoffice-fresh r rhythmbox filezilla uget qbi
 							lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk devtools git jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc \
 							jre17-openjdk-headless jre17-openjdk jdk17-openjdk openjdk17-doc texstudio texmaker texlive-most ntp ufw wget alacarte cups cups-pdf simple-scan alsa-utils pulseaudio pavucontrol \
 							pulseaudio-alsa usbutils simple-scan cups cups-pdf docker xfce4-whiskermenu-plugin thunderbird papirus-icon-theme gvfs ntfs-3g firefox chromium jq \
-							virtualbox audacity godot maxima qt5ct qt6ct noto-fonts cantarell-fonts kvantum
+							virtualbox audacity godot maxima qt5ct qt6ct noto-fonts cantarell-fonts kvantum engrampa
 							
 sudo systemctl enable lightdm
 sudo systemctl enable NetworkManager
